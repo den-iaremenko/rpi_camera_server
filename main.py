680c4533582d3f1fd2fb90092bbe1fc56f93128c
Denys Iaremenko <denysiaremenko@gmail.com>
2024-04-04 22:48:15 +0300

    initial commit

--- main.py
+++ main.py
@@ -39,13 +39,26 @@ async def video_generator():
 
     cap.release()
 
+
+def generate_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
 @app.get("/video_feed")
 async def video_feed():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
     # return StreamingResponse(video_generator(), media_type="multipart/x-mixed-replace; boundary=frame")
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    ret, buffer = cv2.imencode('.jpg', frame)
-    return Response(content=buffer.tobytes(), media_type="image/jpeg")
+    # cap = cv2.VideoCapture(0)
+    # ret, frame = cap.read()
+    # ret, buffer = cv2.imencode('.jpg', frame)
+    # return Response(content=buffer.tobytes(), media_type="image/jpeg")
 
 @app.get("/photo")
 async def photo():
